7e6594f714d3f334322811fb27672b2225b3ea5f
wandrey7 <123254136+wandrey7@users.noreply.github.com>
2025-11-10 14:37:22 -0300

    fix fetchData function

--- src/pages/interactive-map/api.js
+++ src/pages/interactive-map/api.js
@@ -1,19 +1,21 @@
 async function fetchData() {
-            try {
-                const response = await fetch('https://acesso-livre-api.onrender.com/api/locations'); // Endereço da API FastAPI
+  try {
+    const response = await fetch(
+      "https://acesso-livre-api.onrender.com/api/locations"
+    );
 
-                if (response.ok) {
-                    const data = await response.json();
-                    console.log('Location:');
-                    data.forEach(location => {
-                        console.log(location);
-                    });
-                } else {
-                    throw new Error('Erro ao fazer requisição: ' + response.status);
-                }
-            } catch (error) {
-                console.error('Erro na requisição:', error);
-            }
-        }
+    if (response.ok) {
+      const data = await response.json();
+      console.log("Locations:");
+      data.locations.forEach((location, index) => {
+        console.log(`Index ${index}:`, location);
+      });
+    } else {
+      throw new Error("Erro ao fazer requisição: " + response.status);
+    }
+  } catch (error) {
+    console.error("Erro na requisição:", error);
+  }
+}
 
 window.onload = fetchData;
